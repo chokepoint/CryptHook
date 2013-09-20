31050588190e88382dd9e02a0d8107f9e9ac0579
Chokepoint <admin@chokepoint.net>
2013-09-20 00:07:39 -0400

    Changed cipher to AES 256 in GCM mode. Crypto is now authenticated. Added dynamic IVs.

--- README.md
+++ README.md
@@ -7,13 +7,15 @@ hooking the base system calls for network communication send/sendto
 and recv/recvfrom. Crypthook will work with any existing application 
 that relies on these system calls.
 
+Crypto
+-----------------------------
 
-Tested Algorithms
-----------------------------
-
-* Blowfish
-* Advanced Encryption Standard
-
+CryptHook relies on AES in GCM mode using a 256 bit key. Keys are 
+generated from passphrases using PBKDF2. IVs are constructed on the 
+fly using random bytes of data, and the same key derivation technique 
+to reconstruct the initialization vector on the receiving end in order 
+to keep overhead to a minimum. Authentication of each packet is also 
+verified.
 
 Dependencies
 -----------------------------
@@ -35,11 +37,5 @@ Example Use
 	
 	Client
 	$ LD_PRELOAD=./crypthook.so CH_KEY=donthackmebro ncat server 5000
-
-
-PCAP of tunneled SSH session
------------------------------
-
-![SSH inside of tunnel](http://i.imgur.com/7uTSW2q.png)
 
 [www.chokepoint.net](http://www.chokepoint.net)
--- crypthook.c
+++ crypthook.c
@@ -6,6 +6,8 @@
  * Example:
  * $ LD_PRELOAD=crypthook.so CH_KEY=omghax ncat -l -p 5000
  * $ LD_PRELOAD=crypthook.so CH_KEY=omghax ncat localhost 5000
+ * Packet Format:
+ * [algo][iv][hmac][payload]
  */
 #define _GNU_SOURCE
 
@@ -15,7 +17,10 @@
 #include <rhash/rhash.h>
 #include <openssl/evp.h>
 #include <openssl/sha.h>
+#include <openssl/rand.h>
 #include <string.h>
+
+#include <sys/stat.h>
 
 /* Use these to link to actual functions */
 static ssize_t (*old_recv)(int sockfd, void *buf, size_t len, int flags);
@@ -26,14 +31,19 @@ static ssize_t (*old_sendto)(int sockfd, void *buf, size_t len, int flags, const
 #define KEY_VAR "CH_KEY"
 #define PASSPHRASE "Hello NSA"
 #define MAX_LEN 65535
+					
+#define KEY_SIZE 32  	
+	
+#define PACKET_HEADER 0x17		// Added to front of each packet
 
-// Zeroes are good enough for SSH, its good enough for this example
-// CHANGE THIS to something clever
-#define IVEC "\0\0\0\0\0\0\0\0" 
+#define IV_RAND 8 // use 8 bytes of random data to derive the IV on the receiving end
+#define IV_SALT "changeme" // Used for deriving the IV
+#define IV_SIZE 12
 
-#define BLOCK_CIPHER EVP_aes_256_cbc() 	// EVP_aes_256_cbc() and EVP_bf_cbc() have been tested
-#define BLOCK_SIZE 16 					// Blowfish = 8 AES = 16
-#define KEY_SIZE 32  					// Blowfish is variable, lets go w/ 256 bits
+// 1 byte packet identifier
+// 8 bytes random data
+// 16 bytes authentication
+#define HEADER_SIZE 25 
 
 // Used in PBKDF2 key generation
 #define ITERATIONS 1000					
@@ -54,88 +64,119 @@ void gen_key(char *phrase, int len) {
 	}
 }
 
+void gen_iv(int new, char *iv, int len, unsigned char *random_data) {
+	// Generate random bytes if we're sending
+	if (new == 1) {
+		RAND_bytes(random_data,IV_RAND);
+	} 
+	
+	PKCS5_PBKDF2_HMAC_SHA1(random_data,IV_RAND,IV_SALT,strlen(IV_SALT),ITERATIONS,IV_SIZE,(unsigned char *)iv);
+}
+
 int encrypt_data(char *in, int len, char *out) {
 	unsigned char outbuf[MAX_LEN];
 	unsigned char temp[MAX_LEN];
+	unsigned char iv[IV_SIZE];
+	unsigned char key[KEY_SIZE];
+	unsigned char random_data[IV_RAND];
+	unsigned char tag[16];
 	
-	char key[KEY_SIZE];
-	int pad=0, tmplen=0, outlen=0;
-	
-	// Null out temp so that padding is completed with null bytes.
+	unsigned char *step;
+	int tmplen=0, outlen=0;
+
+	// copy plain text message into temp
 	memset(temp,0x00,MAX_LEN);
 	memcpy(temp,in,len);
 	
 	gen_key(key,KEY_SIZE); // Determine key based on environment 
+	gen_iv(1,iv,IV_SIZE,random_data);
 	
-	EVP_CIPHER_CTX ctx;
-	EVP_CIPHER_CTX_init (&ctx);
-	EVP_EncryptInit_ex (&ctx, BLOCK_CIPHER , NULL, (const unsigned char *)key, (const unsigned char *)IVEC);
+	EVP_CIPHER_CTX *ctx;
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_CIPHER_CTX_init (ctx);
+	EVP_EncryptInit_ex (ctx, EVP_aes_256_gcm() , NULL, (const unsigned char *)key, (const unsigned char *)iv);
 
-	/* How many bytes do we need? */
-	pad = BLOCK_SIZE - (len % BLOCK_SIZE);
-
-	if (!EVP_EncryptUpdate (&ctx, outbuf, &outlen, (const unsigned char *)temp, len+pad)) {
+	if (!EVP_EncryptUpdate (ctx, outbuf, &outlen, (const unsigned char *)temp, len)) {
 		fprintf(stderr, "[!] Error in EVP_EncryptUpdate()\n");
-		EVP_CIPHER_CTX_cleanup (&ctx);
+		EVP_CIPHER_CTX_cleanup (ctx);
 		return 0;
 	}
 
-	if (!EVP_EncryptFinal_ex (&ctx, outbuf + outlen, &tmplen)) {
+	if (!EVP_EncryptFinal_ex (ctx, outbuf + outlen, &tmplen)) {
 		fprintf(stderr, "[!] Error in EVP_EncryptFinal_ex()\n");
-		EVP_CIPHER_CTX_cleanup (&ctx);
+		EVP_CIPHER_CTX_cleanup (ctx);
 		return 0;
 	}
 	
-	memcpy(out,outbuf,outlen+tmplen);
+	EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, 16, tag);
 	
-	EVP_CIPHER_CTX_cleanup (&ctx);
-	return outlen+tmplen;
+	out[0]=PACKET_HEADER;
+	step=&out[1];	
+	memcpy(step,random_data,IV_RAND);
+	step+=IV_RAND;
+	memcpy(step,tag,sizeof(tag));
+	step+=sizeof(tag);
+	memcpy(step,outbuf,outlen+tmplen);
+	
+	EVP_CIPHER_CTX_cleanup (ctx);
+	return outlen+tmplen+HEADER_SIZE;
 }
 
 int decrypt_data(char *in, int len, char *out) {
 	unsigned char outbuf[MAX_LEN];
+	unsigned char iv[IV_SIZE];
+	unsigned char key[KEY_SIZE];
+	unsigned char random_data[IV_RAND];
+	unsigned char tag[16];
+	char *step;
 	
-	char key[KEY_SIZE];
 	int tmplen=0, outlen=0;
 	
 	memset(outbuf,0x00,MAX_LEN);
 	
+	// header information
+	step=in+1;
+	memcpy(random_data,step,IV_RAND);
+	step+=IV_RAND;
+	memcpy(tag,step,16);
+	step+=16;
+
 	gen_key(key,KEY_SIZE); // Determine key based on environment 
+	gen_iv(0,iv,IV_SIZE,random_data);
 	
-	EVP_CIPHER_CTX ctx;
-	EVP_CIPHER_CTX_init (&ctx);
-	EVP_DecryptInit_ex (&ctx, BLOCK_CIPHER , NULL, (const unsigned char *)key, (const unsigned char *)IVEC);
-
-	if (!EVP_DecryptUpdate (&ctx, outbuf, &outlen, (const unsigned char *)in, len)) {
+	EVP_CIPHER_CTX *ctx;
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_CIPHER_CTX_init (ctx);
+	EVP_DecryptInit_ex (ctx, EVP_aes_256_gcm() , NULL, (const unsigned char *)key, (const unsigned char *)iv);
+	
+	EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_IVLEN, IV_SIZE, NULL);
+	
+	if (!EVP_DecryptUpdate (ctx, outbuf, &outlen, (const unsigned char *)step, len)) {
 		fprintf(stderr, "[!] Error in EVP_DecryptUpdate()\n");
-		EVP_CIPHER_CTX_cleanup (&ctx);
+		EVP_CIPHER_CTX_cleanup (ctx);
 		return 0;
 	}
 
-	if (!EVP_DecryptFinal_ex (&ctx, outbuf + outlen, &tmplen)) {
-		fprintf(stderr, "[!] Error in EVP_DecryptFinal_ex()\n");
-		EVP_CIPHER_CTX_cleanup (&ctx);
+	EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, sizeof(tag), tag);
+
+	if (!EVP_DecryptFinal_ex (ctx, outbuf + outlen, &tmplen)) {
+		fprintf(stderr, "[!] Error in EVP_DecryptFinal_ex(). Possible foul play involved.\n");
+		EVP_CIPHER_CTX_cleanup (ctx);
 		return 0;
 	}
 	
-	EVP_CIPHER_CTX_cleanup (&ctx);
+	EVP_CIPHER_CTX_cleanup (ctx);
 	
-	// this is hacky, but works... clean it up if you like
-	char *step=outbuf+len;
-	int i = len+1;
-	while (*step == 0x00 || *step == 0x10) {
-		--step; --i;
-	}
+	memcpy(out,outbuf,outlen+tmplen);
 	
-	memcpy(out,outbuf,i);
-	
-	return i;
+	return len;
 }
 
 /* Hook recv and decrypt the data before returning to the program */
 ssize_t recv(int sockfd, void *buf, size_t len, int flags) {
 	char outbuf[MAX_LEN];
 	char temp[MAX_LEN];
+	char *step;
 	
 	int outlen, ret;
 
@@ -154,7 +195,13 @@ ssize_t recv(int sockfd, void *buf, size_t len, int flags) {
 		return ret;
 	}
 
-	outlen = decrypt_data((char *)temp,ret,&outbuf[0]);
+	if (temp[0] != PACKET_HEADER) {
+		fprintf(stderr,"[!] Client not using CryptHook\n");
+		return 0;
+	}
+	step=&temp[0];
+
+	outlen = decrypt_data(step,ret - HEADER_SIZE,&outbuf[0]);
 
 	memcpy((void*)buf,(void*)outbuf,(size_t)outlen);
 	
@@ -165,6 +212,7 @@ ssize_t recv(int sockfd, void *buf, size_t len, int flags) {
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *src_addr, socklen_t *addrlen) {	
 	char outbuf[MAX_LEN];
 	char temp[MAX_LEN];
+	char *step;
 	
 	int outlen, ret;
 
@@ -183,7 +231,13 @@ ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockaddr *
 		return ret;
 	}
 
-	outlen = decrypt_data((char *)temp,ret,&outbuf[0]);
+	if (temp[0] != PACKET_HEADER) {
+		fprintf(stderr,"[!] Client not using same crypto algorithm\n");
+		return 0;
+	}
+	step=&temp[0];
+
+	outlen = decrypt_data(step,ret-HEADER_SIZE,&outbuf[0]);
 
 	memcpy((void*)buf,(void*)outbuf,(size_t)outlen);
 	
